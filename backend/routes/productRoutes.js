57d2a8da689b3b3218e0530f20f67c4f095a1f40
kenatilahun <kenatilahun7@gmail.com>
2026-02-23 14:48:17 +0300

    productdetail

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 // import { protect, admin } from '../middleware/authmiddleware.js';
 
-import { createProduct, getProducts, getProductsByCategory } from '../controllers/productController.js';
+import { createProduct, getProductById, getProducts, getProductsByCategory } from '../controllers/productController.js';
 import upload from '../middleware/uploadMiddleware.js';
 // routes/categoryRoutes.js
 
@@ -10,6 +10,7 @@ const router = express.Router();
 
 router.get("/", getProducts);
 router.get("/category/:category", getProductsByCategory);
+router.get("/:id", getProductById);
 // router.post('/upload', protect, admin, upload.single('image'), createProduct);
 router.post('/production', upload.single("image"),createProduct);
 export default router;
